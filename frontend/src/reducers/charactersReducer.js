aca119c3863ea736d1230c8655c900c9bb6f28e9
PrincetonJRose <decoyzpr@gmail.com>
2020-02-27 14:01:07 -0500

    character sprite now faces correct direction and goes through the different sprites on the sprite sheet

--- frontend/src/reducers/charactersReducer.js
+++ frontend/src/reducers/charactersReducer.js
@@ -1,6 +1,6 @@
 import * as Vars from '../config/constantVariables'
 
-export default (state = { currentParty: [], playerCharacter: { position: { x: 0, y: 0 }, layoutPosition: { x: 0, y: 0 } }, }, action) => {
+export default (state = { currentParty: [], playerCharacter: { position: { x: 0, y: 0 }, layoutPosition: { x: 0, y: 0 }, spriteHeight: 40, spriteWidth: 40, spriteSheetX: -1, spriteSheetY: -1 }, }, action) => {
     switch (action.type) {
 
         case "MOVE_PLAYER":
@@ -8,22 +8,34 @@ export default (state = { currentParty: [], playerCharacter: { position: { x: 0,
             let y_axis = state.playerCharacter.position.y
             let x_position = state.playerCharacter.layoutPosition.x
             let y_position = state.playerCharacter.layoutPosition.y
+            let spriteSheetX = state.playerCharacter.spriteSheetX
+            let spriteSheetY = state.playerCharacter.spriteSheetY
             let key = action.location.keyCode
             let layout = action.location.layout
             let distance = Vars.playerSpriteSize
-            if ( key === 37 && x_axis > 0 && (layout[y_position][x_position - 1] === null || layout[y_position][x_position - 1] === 'G') ) {
+            
+            if ( key === 37 && x_axis > 0 && ( layout[y_position][x_position - 1] === null || layout[y_position][x_position - 1] === 'G' ) ) {
                 x_axis -= distance
                 x_position -= 1
-            } else if ( key === 38 && y_axis > 0 && (layout[y_position - 1][x_position] === null || layout[y_position - 1][x_position] === 'G') ) {
+                spriteSheetY = -81
+            } else if ( key === 38 && y_axis > 0 && ( layout[y_position - 1][x_position] === null || layout[y_position - 1][x_position] === 'G' ) ) {
                 y_axis -= distance
                 y_position -= 1
-            } else if ( key === 39 && x_axis < Vars.maxMapWidth - distance  && (layout[y_position][x_position + 1] === null || layout[y_position][x_position + 1] === 'G') ) {
+                spriteSheetY = -41
+            } else if ( key === 39 && x_axis < Vars.maxMapWidth - distance  && ( layout[y_position][x_position + 1] === null || layout[y_position][x_position + 1] === 'G' ) ) {
                 x_axis += distance
                 x_position += 1
-            } else if ( key === 40 && y_axis < Vars.maxMapHeight - distance && (layout[y_position + 1][x_position] === null || layout[y_position + 1][x_position] === 'G') ) {
+                spriteSheetY = -121
+            } else if ( key === 40 && y_axis < Vars.maxMapHeight - distance && ( layout[y_position + 1][x_position] === null || layout[y_position + 1][x_position] === 'G' ) ) {
                 y_axis += distance
                 y_position += 1
+                spriteSheetY = -1
             }
+            
+            spriteSheetX -= 40
+            if ( spriteSheetX < -290 )
+                spriteSheetX = -1
+
             return {
                 ...state,
                 playerCharacter: {
@@ -35,6 +47,8 @@ export default (state = { currentParty: [], playerCharacter: { position: { x: 0,
                         x: x_position,
                         y: y_position
                     },
+                    spriteSheetX: spriteSheetX,
+                    spriteSheetY: spriteSheetY,
                 }
             }
 
